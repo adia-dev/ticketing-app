0cd0877343e3d094e8997d6886f5c815151b62b7
🦀 adia-dev <abdoudu78130@gmail.com>
2025-02-10 22:33:40 +0100

    🎉 Created the project (vite tailwind configured)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <p>
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+    </p>
+  );
+}
+
+export default App;
